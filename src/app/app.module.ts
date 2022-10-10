e00d616012401ac622dc91951ca64af05a7772f7
kishore951 <kishoresakthivel951@gmail.com>
2022-10-10 20:19:58 +0530

    okk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddCourseComponent } from './add-course/add-course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddCourseComponent
   ],
   imports: [
     BrowserModule,
